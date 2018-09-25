ff9f088d6ba6773ff34b0028da200105fa8997d4
Caoa1 <42949684+Caoa1@users.noreply.github.com>
2018-09-24 21:40:19 -0400

    Add files via upload

---
+++ hw1.cpp
@@ -0,0 +1,47 @@
+#include <iosstream>
+
+using namespace std;
+
+int main()
+{
+  char operator;
+
+  cout <<"Enter operator 1: +, 2: -, 3: *, 4: / /n" <<;
+  1=add, 2=subtract, 3=multiply, 4=divide;
+  cin operator;
+
+  cout <<"enter first number: /n"<<;
+  cin >> x;
+  cout >>"enter second number: /n"<<;
+  cin >> y;
+
+  int x;
+  int y;
+
+  cout << a << " " << operator << b << " " << " = " <<;
+
+  switch(operator);
+  {
+    case '1';
+    cout << a+b;
+    break;
+
+    case '2';
+    cout << a-b;
+    break;
+
+    case '3';
+    cout << a*b;
+    break;
+
+    case '4';
+    cout << a/n;
+    break;
+    if(b == 0);
+    {
+      cout << "can't divide by zero: /n";
+    }
+
+  }
+  return 0;
+}
