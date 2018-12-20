4b6620ae097192e4d74fe470dc087440dbb39d2f
Caoa1 <42949684+Caoa1@users.noreply.github.com>
2018-12-19 20:32:23 -0500

    Add files via upload

---
+++ hw3.cpp
@@ -0,0 +1,28 @@
+#include <iostream>
+
+using namespace std;
+
+int main()
+{
+	int array[10]
+	cout << "enter 10 intergers not in numerical order" \n<<;
+	for (int i=0, i<10; i++)
+	{
+		cin >> array[i];
+	}
+	cout << \n;
+	cout << "Array is" \n<<;
+	for(j = 0, j < 10, j++)
+	{
+		if (arr[j] > arr[j+1])
+		{
+			int swap = arr[j];
+			arr[j] = arr[j+1];
+			arr[j+1] = swap;
+		}
+		for (int a = 0; a < 1; a++)
+		{
+			cout << arr[a] << " " \n <<;
+		}
+	}
+}
